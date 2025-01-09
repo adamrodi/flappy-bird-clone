e29232ca0f3ea58b16cb271146a317d35f55e962
adamrodi <adamcrodi123@yahoo.com>
2025-01-09 13:19:52 -0600

    add window without background

---
+++ src/App.java
@@ -0,0 +1,18 @@
+package src;
+
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        int boardWidth = 360;
+        int boardHeight = 640;
+
+        // build window
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+    }
+}
---
+++ src/FlappyBird.java
@@ -0,0 +1,9 @@
+package src;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+
+public class FlappyBird {
+    
+}
